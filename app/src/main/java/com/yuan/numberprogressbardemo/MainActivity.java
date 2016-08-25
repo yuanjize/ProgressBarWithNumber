8b717681976129853e6b8e244eced7381dc42291
yuanjize <1224500078@qq.com>
2016-08-25 19:35:57 +0800

    init

---
+++ app/src/main/java/com/yuan/numberprogressbardemo/MainActivity.java
@@ -0,0 +1,39 @@
+package com.yuan.numberprogressbardemo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends AppCompatActivity {
+    private MyProgressBar bar;
+    private Timer timer;
+    private int i = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        bar = (MyProgressBar) findViewById(R.id.number_progress_bar);
+        bar.setMaxValue(5000);
+        timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        bar.setProgress(i++);
+                    }
+                });
+            }
+        }, 1000, 100);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        timer.cancel();
+    }
+}
